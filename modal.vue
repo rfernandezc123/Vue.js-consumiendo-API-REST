8920dca0a6a8253fb3e940cb8d4352d0f75be88a
Rony Fernandez Carranza <87103797+rfernandezc123@users.noreply.github.com>
2024-05-22 15:13:07 -0500

    Create modal.vue

---
+++ modal.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-modal
+      ref="modal-appointments-grid"
+      size="lg"
+      @hidden="\$emit('hidden')"
+      no-close-on-backdrop
+    >
+      <template #modal-title>
+        <span class="text-uppercase font-weight-bolder"> Title </span>
+      </template>
+      <template #default> </template>
+      <template #modal-footer>
+        <b-row class="d-flex justify-content-end">
+          <b-button variant="primary"> SAVE </b-button>
+        </b-row>
+      </template>
+    </b-modal>
+  </div>
+</template>
+<script>
+export default {
+  name: "ModalAppointmentsGrid",
+  data() {
+    return {};
+  },
+  computed: {},
+  created() {},
+  mounted() {
+    this.toggleModal("modal-appointments-grid");
+  },
+  methods: {},
+};
+</script>
+<style lang="scss">
+</style>
