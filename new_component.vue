b7e5c4476ac5c13538d8aa658f192d89a94ba6c8
Rony Fernandez Carranza <87103797+rfernandezc123@users.noreply.github.com>
2024-05-22 15:14:09 -0500

    Create new_component.vue

---
+++ new_component.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: "AppointmentsDashboardCrm",
+  data() {
+    return {};
+  },
+  computed: {},
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+<style lang="scss">
+</style>
